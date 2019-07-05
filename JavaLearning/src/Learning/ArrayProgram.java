a96a037d501a6e7547e331e3fc91b89141eecbfc
kavithakonalc <46570559+kavithakonalc@users.noreply.github.com>
2019-07-05 22:47:50 +0530

    Added array program

---
+++ JavaLearning/src/Learning/ArrayProgram.java
@@ -0,0 +1,14 @@
+package Learning;
+
+public class ArrayProgram {
+
+	public static void main(String[] args) {
+		//int[] a=new int[4];
+		int[] a= {1,2,3,4};
+		for(int i=0 ; i<a.length ; i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+
+}
