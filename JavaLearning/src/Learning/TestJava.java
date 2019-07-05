7a10df7d6f7a7a34b8f0bece0f40c1a751177553
kavithakonalc <kavitha.konal@gmail.com>
2019-07-05 16:58:59 +0530

    test

--- JavaLearning/src/Learning/TestJava.java
+++ JavaLearning/src/Learning/TestJava.java
@@ -3,7 +3,7 @@ package Learning;
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("my name is kavitha");
+		System.out.println("my name is savitha");
 
 	}
 
