5b51937d1477e0cf49f56c5c00992ba5f567074f
kavithakonalc <46570559+kavithakonalc@users.noreply.github.com>
2019-07-05 22:50:12 +0530

    changed array name a to arr

--- JavaLearning/src/Learning/ArrayProgram.java
+++ JavaLearning/src/Learning/ArrayProgram.java
@@ -4,9 +4,9 @@ public class ArrayProgram {
 
 	public static void main(String[] args) {
 		//int[] a=new int[4];
-		int[] a= {1,2,3,4};
-		for(int i=0 ; i<a.length ; i++) {
-			System.out.println(a[i]);
+		int[] arr= {1,2,3,4};
+		for(int i=0 ; i<arr.length ; i++) {
+			System.out.println(arr[i]);
 		}
 		
 	}
